52698d38f49627cb9b172cd1bd99bd264c7ad982
= <=>
2023-10-19 22:36:14 -0300

    update

--- js/form.js
+++ js/form.js
@@ -61,8 +61,8 @@ class FormSubmit{
 const formSubmit = new FormSubmit({
     form: "[data-form]",
     button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possível enviar a sua mensagem</h1>"
+    success: "<h3 class='success'>Mensagem enviada!</h3>",
+    error: "<h3 class='error'>Não foi possível enviar a sua mensagem</h3>"
 })
 
 formSubmit.init()
